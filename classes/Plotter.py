24eb3a7f812107183f87ff1e80664dbdee12cd90
Sebastiano Spinali <sebastiano.spinali@cern.ch>
2020-12-07 12:33:41 +0100

    Add profit plots in Plotter class

--- classes/Plotter.py
+++ classes/Plotter.py
@@ -8,12 +8,34 @@ plotter_log.setLevel(logging.INFO)
 
 
 class Plotter(object):
-    def __init__(self, df, output):
+    def __init__(self, df, df_name, output):
         self.df = df
+        self.df_name = df_name
         self.output = output
 
-    def plot_open(self):
-        plot = self.df["Open"].hist()
-        plot.get_figure()
-        figname = os.path.join(self.output, "histo" + ".png")
-        plt.savefig(figname)
+    def plot_profit_template(self):
+        self.plot_profit()
+        self.plot_cum_profit()
+
+    def plot_profit(self):
+        plt.plot(self.df.index,
+                 self.df["daily_profit"],
+                 label=self.df_name)
+        plt.xticks(rotation=45, fontsize=6)
+        plt.yticks(fontsize=6)
+        plt.legend(loc='upper left', frameon=False)
+        figname = os.path.join(self.output, "daily_profit" + ".png")
+        plt.savefig(figname, dpi=200)
+        plt.close()
+
+    def plot_cum_profit(self):
+        plt.plot(self.df.index,
+                 self.df["long_short_profit"],
+                 label=self.df_name)
+        plt.xticks(rotation=45, fontsize=6)
+        plt.yticks(fontsize=6)
+        figname = os.path.join(self.output, "cum_profit" + ".png")
+        plt.savefig(figname, dpi=200)
+        plt.close()
+
+
