5768823e6d9573be7bdabc3345936a9737cafe91
Sebastiano Spinali <sebastiano.spinali@cern.ch>
2021-01-25 12:49:53 +0100

    Major correction. Separate the contribution from different regressor tools. Create new plot functions.

--- plot_main.py
+++ plot_main.py
@@ -15,11 +15,9 @@ logger.setLevel(logging.DEBUG)
 
 parser = argparse.ArgumentParser(description="Load flag to run StockRegression")
 parser.add_argument('-i', '--input', type=str, metavar='', required=True,
-                    help='Specify the path of the root file'
-                    )
+                    help='Specify the path of the root file')
 parser.add_argument('-o', '--output', type=str, metavar='', required=True,
-                    help='Specify the output path'
-                    )
+                    help='Specify the output path')
 args = parser.parse_args()
 
 
@@ -31,30 +29,21 @@ def plot_exe(inpt, output):
     :param output: args.output
     :return: pandas dataframe
     """
+    logger.info("in plot execute")
     dfs_list = []
-    prefit_list = []
     profit_output = create_folder(output, pc["profit_plot_dir"])
     for csv_file in os.listdir(inpt):
         if fnmatch.fnmatch(csv_file, '*.csv'):
-
             # create list of dfs
             csv, csv_string = csv_maker(str(inpt), csv_file)
-            df_temp = pd.read_csv(csv, parse_dates=[mc["date_clm"]], index_col=0)
-            df_prefit = df_temp.copy()
+            df = pd.read_csv(csv, parse_dates=[mc["date_clm"]], index_col=0)
+            df_temp = df.copy()
             df_temp = setting_date_as_index(df_temp)
-
             # plot profit for each stock
             single_model_profit(df_temp, csv_string, profit_output)
             dfs_list.append((df_temp, csv_string))
-            prefit_list.append((df_prefit, csv_string))
-    logger.info("Singular plots done!!")
-
-    # # plot prefit variables
-    # plot_prefit_variables(dfs_list, prefit_output)
-
-    # plot profit for the whole set of stochs
     multiple_profit_plot(dfs_list, profit_output)
-    logger.info("Multiple profit done plots done!!")
+    logger.info("plot execute done!")
     return dfs_list
 
 
@@ -67,6 +56,7 @@ def single_model_profit(df, csv_str, output):
     :param output: output path
     :return: None
     """
+    # logger.info("In singular plots")
     if pc["profit_csv"] in csv_str:
         if pc["rf_csv"] in csv_str:
             output_loc = create_folder(output, csv_str)
@@ -76,6 +66,7 @@ def single_model_profit(df, csv_str, output):
             output_loc = create_folder(output, csv_str)
             pl = Plotter(df, csv_str, output_loc)
             pl.plot_profit_template()
+    # logger.info("Singular plots done!!")
 
 
 def setting_date_as_index(df):
@@ -84,8 +75,9 @@ def setting_date_as_index(df):
     :param df: pandas dataframe
     :return: pandas df indexed
     """
-    df.set_index("Date", inplace=True)
-    return df
+    df_temp = df.copy()
+    df_temp.set_index("Date", inplace=True)
+    return df_temp
 
 
 def multiple_profit_plot(df_list, output):
@@ -97,36 +89,64 @@ def multiple_profit_plot(df_list, output):
     """
     logger.info("in multiple profit plots")
     dfs_profit_list = []
-    # df_total_list = []
     for item in df_list:
         df = item[0]
         df_name = item[1]
         if pc["daily_profit"] in df.columns.to_list():
             dfs_profit_list.append((df, df_name))
-    rf_list, lr_list = create_list_of_same_models(dfs_profit_list)
-    # sorting model lists
-    sorted_rf_list = asc_sort_tuple(rf_list)
-    sorted_lr_list = asc_sort_tuple(lr_list)
-    # profit shift
-    sorted_rf_list = profit_shift(sorted_rf_list)
-    sorted_lr_list = profit_shift(sorted_lr_list)
-    df_total_list = sorted_lr_list + sorted_rf_list
-    # Applying profit plotter class
-    # total
-    pp = ProfitPlotter(df_total_list, "total", output)
-    pp.make_multiple_profit_total_plot()
-    pp.make_multiple_profit_plot_ranking()
-    # rf
-    pp_rf = ProfitPlotter(sorted_rf_list, "random_forest_profit", output)
-    pp_rf.make_multiple_profit_plot()
-    pp_rf.make_multiple_profit_plot_ranking()
-    # lr
-    pp_lr = ProfitPlotter(sorted_lr_list, "linear_regres_profit", output)
-    pp_lr.make_multiple_profit_plot()
-    pp_lr.make_multiple_profit_plot_ranking()
+    rf, lr, gbr, knr, lasso, enr, drt = create_list_of_same_models(dfs_profit_list)
+    make_profit_plot(rf, output, "rf")
+    make_profit_plot(lr, output, "lr")
+    make_profit_plot(lr, output, "gbr")
+    make_profit_plot(knr, output, "knr")
+    make_profit_plot(lasso, output, "lasso")
+    make_profit_plot(enr, output, "enr")
+    make_profit_plot(drt, output, "drt")
+    lr_df = create_unique_df(lr, "lr")
+    rf_df = create_unique_df(rf, "rf")
+    gbr_df = create_unique_df(gbr, "gbr")
+    knr_df = create_unique_df(knr, "knr")
+    lasso_df = create_unique_df(lasso, "lasso")
+    enr_df = create_unique_df(enr, "enr")
+    # drt_df = create_unique_df(drt)
+    df_total_list = lr_df + rf_df + gbr_df + knr_df + lasso_df + enr_df # + drt_df
+    make_profit_plot(df_total_list, output, "total")
+    logger.info("Multiple profit done plots done!!")
+
+
+def create_unique_df(tup, name):
+    lst = []
+    lst1 = []
+    for item in tup:
+        df = item[0]
+        lst.append(df)
+    df = pd.concat(lst)
+    lst1.append((df, name))
+    return lst1
+
+
+def make_profit_plot(model_list, output, model_name):
+    """
+    It produce profit plot
+    :param model_list: model dataframe list
+    :param output: output path
+    :param model_name: model name
+    :return: None
+    """
+    logger.info("... in make {} profit plot".format(model_name))
+    model = ProfitPlotter(model_list, model_name, output)
+    model.make_multiple_profit_plot()
+    model.make_multiple_profit_plot_ranking()
+    logger.info("{} profit plot done!".format(model_name))
 
 
 def profit_shift(df_list):
+    """
+    It shift profit value by looking at the last value of the
+    previous dataframe
+    :param df_list: dataframes list
+    :return: sorted dataframes list
+    """
     shift = 0
     sorted_tuple = []
     for item in df_list:
@@ -144,42 +164,53 @@ def create_list_of_same_models(df_list):
     """
     It split the df list in as many list
     as used models.
-    :param df_list:
+    :param df_list: list of dataframe
     :return: list
     """
+    logger.info("...in create list of same models")
     rf = []
     lr = []
+    gbr = []
+    knr = []
+    lasso = []
+    enr = []
+    drt = []
     for item in df_list:
         df = item[0]
         df_name = item[1]
         if "lr" in df_name:
             lr.append((df, df_name))
-        else:
+            lr = asc_sort_tuple(lr)
+            lr = profit_shift(lr)
+        if "rf" in df_name:
             rf.append((df, df_name))
-    return rf, lr
-
-
-# def plot_prefit_variables(prefit_list, output):
-#     result = pd.DataFrame()
-#     for item in prefit_list:
-#         df = item[0]
-#         df_name = item[1]
-#         if pc["general_csv"] in item[1]:
-#             print("temp df: ", df_name, df.shape, result.shape)
-#             result = pd.concat([result, df])
-#     i = 0
-#     for stock in set(result[mc["ticker"]].values):
-#         i += 1
-#         if i == 10:
-#             break
-#         df_to_plot = result[(result[mc["ticker"]] == stock)]
-#         df_to_plot.sort_index()
-#         stock_output = create_folder(output, stock)
-#         prefplot = prefit_plotter(df_to_plot, stock_output)
-#         prefplot.prefit_plotter_exe()
+            rf = asc_sort_tuple(rf)
+            rf = profit_shift(rf)
+        if "gbr" in df_name:
+            gbr.append((df, df_name))
+            gbr = asc_sort_tuple(gbr)
+            gbr = profit_shift(gbr)
+        if "knr" in df_name:
+            knr.append((df, df_name))
+            knr = asc_sort_tuple(knr)
+            knr = profit_shift(knr)
+        if "lasso" in df_name:
+            lasso.append((df, df_name))
+            lasso = asc_sort_tuple(lasso)
+            lasso = profit_shift(lasso)
+        if "enr" in df_name:
+            enr.append((df, df_name))
+            enr = asc_sort_tuple(enr)
+            enr = profit_shift(enr)
+        elif "drt" in df_name:
+            drt.append((df, df_name))
+            drt = asc_sort_tuple(drt)
+            drt = profit_shift(drt)
+    logger.info("Create list of same models done!")
+    return rf, lr, gbr, knr, lasso, enr, drt
 
 
 if __name__ == '__main__':
-    logger.info("~~~### WELL DONE, plot section is now ACTIVE ###~~~")
-    logger.info("in plot main")
+    logger.info("~~~### plot section is now ACTIVE ###~~~")
     dfs = plot_exe(args.input, args.output)
+    logger.info("WELL DONE, plot main done!")
