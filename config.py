9707ceeb2d932fff13ad692d4d5d8a291e94b723
Sebastiano Spinali <sebastiano.spinali@cern.ch>
2021-02-11 15:26:35 +0100

    minor

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import datetime as dt
 
 main_conf = {
-    "prefit_plot_flag": False,
+    "prefit_plot_flag": True,
     "columns": ["Date", "Close", "Open", "Volume", "High", "Low"],
     "train_window": [
         (dt.datetime.strptime("2000-01-01", "%Y-%m-%d"), dt.datetime.strptime("2004-01-01", "%Y-%m-%d")),
