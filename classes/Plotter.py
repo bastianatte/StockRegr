ec9bbd6df8b774be2a71f768ed6a5387e23bad2c
Sebastiano Spinali <sebastiano.spinali@cern.ch>
2021-04-01 10:48:00 +0200

    Add ensemble and single score table

--- classes/Plotter.py
+++ classes/Plotter.py
@@ -2,9 +2,6 @@ import logging
 import os
 from utils.misc import get_logger
 import matplotlib.pyplot as plt
-# import plotly.graph_objects as go
-# import dataframe_image as dfi
-from pandas.plotting import table
 
 plotter_log = get_logger(__name__)
 plotter_log.setLevel(logging.INFO)
@@ -61,41 +58,45 @@ class Plotter(object):
         ax.axis('off')
         ax.axis('tight')
         tabla = ax.table(cellText=self.df.values, colLabels=self.df.columns, loc='center')
-        tabla.scale(1, 2)
+        tabla.scale(0.7, 0.5)
         tabla.auto_set_font_size(False)
-        tabla.set_fontsize(6)
+        tabla.set_fontsize(4)
         fig.tight_layout()
         figname = os.path.join(self.output, "metrics_table" + ".png")
         plt.savefig(figname, dpi=400)
         plt.close()
 
-    # def plot_metrics_table_2(self):
-    #     print(self.df.head(5))
-    #     print(self.df.columns, self.df.values)
-    #     fig = go.Figure(data=[go.Table(header=dict(values=list(self.df.columns)), cells=dict(values=self.df.values))])
-    #     # fig1 = go.Figure(data=[go.Table(header=dict(values=['A Scores', 'B Scores']),
-    #     # cells=dict(values=[[100, 90, 80, 90], [95, 85, 75, 95]]))])
-    #     figname = os.path.join(self.output, "metrics_table_2" + ".png")
-    #     fig.write_image(figname)
+    def plot_score_table(self):
+        fig, ax = plt.subplots()
+        print(self.df.shape)
+        fig.patch.set_visible(False)
+        ax.axis('off')
+        ax.axis('tight')
+        tabla = ax.table(cellText=self.df.values,
+                         colLabels=self.df.columns,
+                         loc='center')
+        tabla.scale(1, 2)
+        tabla.auto_set_font_size(False)
+        tabla.set_fontsize(9)
+        fig.tight_layout()
+        figname = os.path.join(self.output, "score_table_" + str(self.df_name) + ".png")
+        plt.savefig(figname, dpi=400)
+        plt.close()
+        plotter_log.info("score table done!")
 
-    # def plot_metrics_table_3(self):
-    #     figname = os.path.join(self.output, "metrics_table_3" + ".png")
-    #     dfi.export(self.df, figname)
-    #
-    # def plot_metrics_table_4(self):
-    #     print(self.df.head(5))
-    #     fig, ax = plt.subplots(figsize=(14, 3))  # set size frame
-    #     # df.index = [item.strftime('%Y-%m-%d') for item in df.index]
-    #     ax.xaxis.set_visible(False)  # hide the x axis
-    #     ax.yaxis.set_visible(False)  # hide the y axis
-    #     ax.set_frame_on(False)  # no visible frame, uncomment if size is ok
-    #     tabla = table(ax, self.df, loc='upper right', colWidths=[0.17] * len(self.df.columns))
-    #     # tabla.auto_set_font_size(False)  # Activate set fontsize manually
-    #     # tabla.set_fontsize(12)  # if ++fontsize is necessary ++colWidths
-    #     # tabla.scale(1.2, 1.2)  # change size table
-    #     # plt.savefig('table.png', transparent=True)
-    #     figname = os.path.join(self.output, "metrics_table_4" + ".png")
-    #     plt.savefig(figname, dpi=200)
-    #     plt.close()
-
-
+    def plot_ens_score_table(self):
+        fig, ax = plt.subplots()
+        fig.patch.set_visible(False)
+        ax.axis('off')
+        ax.axis('tight')
+        tabla = ax.table(cellText=self.df.values,
+                         colLabels=self.df.columns,
+                         loc='center')
+        tabla.scale(1, 0.5)
+        tabla.auto_set_font_size(False)
+        tabla.set_fontsize(5)
+        fig.tight_layout()
+        figname = os.path.join(self.output, "score_table_" + str(self.df_name) + ".png")
+        plt.savefig(figname, dpi=400)
+        plt.close()
+        plotter_log.info("score table done!")
